9ff44a7e9a680bcbba0657cc4980a9de68669bec
ravindra rawat <ravinrawat.wd@gmail.com>
2019-01-13 14:47:35 -0700

    addedtext  / logo to footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import reactLogo from '../../assets/images/react.png';
+import reactRouterLogo from '../../assets/images/reactRouter.png';
+import sassLogo from '../../assets/images/sass.png';
+//import reactLogo from '../../assets/images/react.png';
 
 export default function() {
   return (
     <footer className="footer">
-      <h2>Last updated : Jan,2019</h2>
+    <p>This site is work in progress -- Built with :</p>
+    <img src={sassLogo}/>
+     <img src={reactRouterLogo}/>
+    <img src={reactLogo}/>
+   
     </footer>
   );
 }
