20af604cedb6c77d0d747425ad6813fff456575f
ravindra rawat <ravinrawat.wd@gmail.com>
2019-01-12 18:07:25 -0700

    added styles to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Projects() {
   return (
-    <div>
+    <div className="projects">
       <h1>Projects</h1>
+      <div className="projects__list">
+        <div className="projects__item">Project-1</div>
+        <div className="projects__item">Project-1</div>
+        <div className="projects__item">Project-1</div>
+        <div className="projects__item">Project-1</div>
+        <div className="projects__item">Project-1</div>
+        <div className="projects__item">Project-1</div>
+      </div>
     </div>
   );
 }
