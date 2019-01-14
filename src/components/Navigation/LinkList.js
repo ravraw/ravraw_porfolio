246f0cb7cbe2d75ac9cdd42a8e961761ff9f7e76
ravindra rawat <ravinrawat.wd@gmail.com>
2019-01-14 15:56:45 -0700

    changes to nav items

--- src/components/Navigation/LinkList.js
+++ src/components/Navigation/LinkList.js
@@ -5,11 +5,21 @@ import { NavLink } from 'react-router-dom';
 export default function LinkList() {
   return (
     <ul className="link__list">
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/skills">Skills</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <NavLink className="link__item" to="/home">
+        Home
+      </NavLink>
+      <NavLink className="link__item" to="/about">
+        About
+      </NavLink>
+      <NavLink className="link__item" to="/skills">
+        Skills
+      </NavLink>
+      <NavLink className="link__item" to="/projects">
+        Projects
+      </NavLink>
+      <NavLink className="link__item" to="/contact">
+        Contact
+      </NavLink>
     </ul>
   );
 }
