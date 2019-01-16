dd3142261bd90b3df1ea7228f366b32ba6af9ed2
ravindra rawat <ravinrawat.wd@gmail.com>
2019-01-16 12:46:55 -0700

    added contact details

--- src/components/PageViews/Contact.js
+++ src/components/PageViews/Contact.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact">
+      <h1>Contact Details</h1>
+      <p>
+        Email :
+        <a href="mailto:ravinrawat.wd@gmail.com">ravinrawat.wd@gmail.com</a>
+      </p>
+      <p>Phone : 403-903-9057</p>
+      <p>Location : calgary,Alberta</p>
+      <p>Facebook: </p>
     </div>
   );
 }
--- src/components/PageViews/Skills.js
+++ src/components/PageViews/Skills.js
@@ -3,11 +3,37 @@ import React from 'react';
 export default function Skills() {
   return (
     <div className="skills">
-      <h1>Skills summary</h1>
+      <h1>Technologies Worked with</h1>
       <div className="skills__list">
-        <div className="skills__item">front-end</div>
-        <div className="skills__item">back-end</div>
-        <div className="skills__item">database</div>
+        <div className="skills__item">
+          <h3>front-end</h3>
+          <ul>
+            <li>Javascript</li>
+            <li>html5</li>
+            <li>css3</li>
+            <li>sass/scss</li>
+            <li>Bootstrap</li>
+            <li>React</li>
+            <li>React router</li>
+            <li>Jest / Enzime(testing)</li>
+            <li>Webpack / Babel (Bundling)</li>
+            <li>GIT (version control)</li>
+          </ul>
+        </div>
+        <div className="skills__item">
+          <h3>back end</h3>
+          <ul>
+            <li>NodeJs</li>
+            <li>Express</li>
+            <li>Ruby </li>
+            <li>Ruby on Rails</li>
+            <li>Restfull Api</li>
+            <li>GraphQL</li>
+            <li>PostgresQl</li>
+            <li>MongoDB</li>
+            <li>KnexJs</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
