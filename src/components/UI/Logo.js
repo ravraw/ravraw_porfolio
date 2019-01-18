d2a28c6115db586de5a748b64ee923c645ef646a
ravindra rawat <ravinrawat.wd@gmail.com>
2019-01-17 20:19:10 -0700

    did scss code refactor

--- src/components/UI/Logo.js
+++ src/components/UI/Logo.js
@@ -3,8 +3,8 @@ import logo from '../../assets/images/logo.png';
 
 export default function Logo() {
   return (
-    <div className="logo__box">
-      <div className="logo">ravraw.io</div>
+    <div className="header__logo_box">
+      <div className="header__logo">ravraw.io</div>
     </div>
   );
 }
